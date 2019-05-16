b34b5f0ad6f1fb9a6e8746af6c7327f7aae00123
Bradshaw <Tony.Bradshaw@cmc.com>
2019-05-16 15:13:46 -0500

    App updates

---
+++ src/Char/Char.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Char = (props) => {
+    const style = {
+        background: '#f1f5fb',
+        border: '1px solid #000',
+        color: '#3b68b5',
+        display: 'inline-block',
+        fontWeight: 'bold',
+        margin: '0 2px 20px',
+        padding: ' 10px 20px',
+        textAlign: 'center',
+        textTransform: 'uppercase'
+    };
+
+    const spacer = {
+        background: 'none',
+        display: 'inline-block',
+        margin: '0 2px',
+        padding: ' 10px'
+    }
+
+    return (
+        <div 
+            style={props.character !== ' ' ? style : spacer}
+            onClick={props.clicked}>
+            {props.character}
+        </div>
+    );
+};
+
+
+export default Char;
---
+++ src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Person.css';
+import Validation from '../Validation/Validation';
+
+const Person = (props) => {
+    return (
+        <div>
+            <div className="person">
+                <p onClick={props.click}>I'm {props.name} and I am {props.age > 1 ? `${props.age} years` : `a ${props.age} year`} old.</p>
+                {props.children ? <p>{props.children}</p> : ''}
+                <input type="text" onChange={props.changed} value={props.name} />
+                <Validation inputLength={props.name.length} />
+            </div>
+            {props.nameList}
+        </div>
+    );
+};
+
+export default Person;
